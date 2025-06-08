8d938b7686d6951f79de439c3975c01b45b9da29
luka1231123 <luka04rekhviashvili@gmail.com>
2025-06-08 17:58:15 +0400

    modified:   __pycache__/config.cpython-311.pyc 	modified:   __pycache__/population.cpython-311.pyc 	modified:   config.py 	__pycache__/hitl_pygame.cpython-311.pyc 	hitl_pygame.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 K          = 20                    # slices
 B_MIN, B_MAX = 0.005, 0.03         # radii bounds (m)
 
-# GA
 N          = 40                    # pop size
 G          = 40                   # generations
 N_E        = 5                    # parents kept
@@ -9,7 +8,6 @@ N_EK       = 2                     # elites copied intact
 SIGMA      = 0.10                  # 10 % of range
 W_A, W_H   = 0.1, 0.9              # default weights
 
-# physicsp
 RHO_MAT    = 7850                 # kg/m³ density
 H          = 0.002                # m
 RHO_AIR    = 1.2
@@ -19,10 +17,8 @@ OMEGA0     = 300                  # rad/s
 
 EPS     = 1e-12 # small constant to avoid divide-by-zero
 
-# number of new random shapes each generation
 N_IMMIGRANTS = 4
 
-# BLX-alpha parameter for crossover
 BLX_ALPHA = 0.3
 
 H_DECAY = 0.95
